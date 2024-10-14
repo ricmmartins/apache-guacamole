dbceae758ac563a0675101db9d64b8f31c98fa69
Ricardo Martins <44813563+ricmmartins@users.noreply.github.com>
2024-10-14 11:13:15 -0400

    Update guac-docker-install-2.sh

--- guac-docker-install-2.sh
+++ guac-docker-install-2.sh
@@ -3,17 +3,14 @@
 # Log the output for debugging purposes
 exec > >(tee /var/log/guac-docker-install.log|logger -t guac-docker-install -s 2>/dev/console) 2>&1
 
-# Install required packages
-echo "Updating packages and installing required software..."
+# Install Docker and Docker Compose
 sudo apt-get update -y
 sudo apt-get install -y docker.io docker-compose
 
 # Create a directory for Docker Compose
-echo "Creating directory for Docker Compose..."
 mkdir -p /home/azureuser/guacamole
 
 # Create a Docker Compose file
-echo "Creating Docker Compose file for Guacamole deployment..."
 cat <<EOF > /home/azureuser/guacamole/docker-compose.yml
 version: '3'
 services:
@@ -31,11 +28,11 @@ services:
     ports:
       - "8080:8080"
     environment:
-      MYSQL_HOSTNAME: "${MYSQL_HOST}"
+      MYSQL_HOSTNAME: "$MYSQL_HOST"
       MYSQL_PORT: "3306"
-      MYSQL_DATABASE: "${MYSQL_DB_NAME}"
-      MYSQL_USER: "${MYSQL_USERNAME}"
-      MYSQL_PASSWORD: "${MYSQL_PASSWORD}"
+      MYSQL_DATABASE: "$MYSQL_DB_NAME"
+      MYSQL_USER: "$MYSQL_USERNAME"
+      MYSQL_PASSWORD: "$MYSQL_PASSWORD"
       GUACAMOLE_HOME: "/etc/guacamole"
       GUACAMOLE_USERNAME: "guacadmin"
       GUACAMOLE_PASSWORD: "guacadmin"
@@ -43,14 +40,19 @@ services:
       - guacd
 EOF
 
-# Set correct permissions
+# Set permissions
 sudo chown -R azureuser:azureuser /home/azureuser/guacamole
 
 # Change to the directory
 cd /home/azureuser/guacamole
 
 # Start the Docker Compose services
-echo "Starting Docker Compose services..."
 sudo docker-compose up -d
+
+# Wait for containers to be ready
+sleep 15
+
+# Import MySQL schema
+sudo docker exec guacamole /opt/guacamole/bin/initdb.sh | mysql -h $MYSQL_HOST -u $MYSQL_USERNAME -p$MYSQL_PASSWORD $MYSQL_DB_NAME
 
 echo "Guacamole setup complete."
